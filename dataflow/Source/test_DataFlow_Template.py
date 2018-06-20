bd46eee0d64546cb10739bc78a8f2b1a4d37b803
Ryan Morris <ryan.c.morris1@gmail.com>
2018-06-20 14:23:13 +1000

    Add test

--- dataflow/Source/test_DataFlow_Template.py
+++ dataflow/Source/test_DataFlow_Template.py
@@ -3,28 +3,50 @@ import DataFlow_Template;
 
 class DataflowTest(unittest.TestCase):
 
+    testCollection = [{'PrevRefIPs': '12', 'TLD': 'au', 'IDN_Domain': '7', 'PrevGlobalRank': '9', 'TldRank': '2',
+                       'Domain': '3', 'PrevTldRank': '10', 'RefIPs': '6', 'RefSubNets': '5', 'IDN_TLD': '8',
+                       'GlobalRank': '1', 'PrevRefSubNets': '11'},
+                      {'PrevRefIPs': '12', 'TLD': 'com', 'IDN_Domain': '7', 'PrevGlobalRank': '9', 'TldRank': '2',
+                       'Domain': '3', 'PrevTldRank': '10', 'RefIPs': '6', 'RefSubNets': '5', 'IDN_TLD': '8',
+                       'GlobalRank': '1', 'PrevRefSubNets': '11'},
+                      {'PrevRefIPs': '12', 'TLD': 'au', 'IDN_Domain': '7', 'PrevGlobalRank': '9', 'TldRank': '2',
+                       'Domain': '3', 'PrevTldRank': '10', 'RefIPs': '6', 'RefSubNets': '5', 'IDN_TLD': '8',
+                       'GlobalRank': '1', 'PrevRefSubNets': '11'}]
+
     def test_Split_shouldReturnRecordFromLine(self):
+        SCHEMA = 'GlobalRank:INTEGER,TldRank:INTEGER,Domain:STRING,TLD:STRING,RefSubNets:INTEGER,RefIPs:INTEGER,IDN_Domain:STRING,' \
+                 'IDN_TLD:STRING,PrevGlobalRank:INTEGER,PrevTldRank:INTEGER,PrevRefSubNets:INTEGER,PrevRefIPs:INTEGER'
         split = DataFlow_Template.Split()
         expected = {'PrevRefIPs': '12', 'TLD': '4', 'IDN_Domain': '7', 'PrevGlobalRank': '9', 'TldRank': '2',
                           'Domain': '3', 'PrevTldRank': '10', 'RefIPs': '6', 'RefSubNets': '5', 'IDN_TLD': '8', 'GlobalRank': '1', 'PrevRefSubNets': '11'}
-        result = split.process("1,2,3,4,5,6,7,8,9,10,11,12")[0]
+        result = split.process("1,2,3,4,5,6,7,8,9,10,11,12", SCHEMA)[0]
         self.assertDictEqual(expected, result)
 
     def test_CountTLDs_shouldReturnTLDsWithCount(self):
-        testCollection = [{'PrevRefIPs': '12', 'TLD': 'au', 'IDN_Domain': '7', 'PrevGlobalRank': '9', 'TldRank': '2',
-                           'Domain': '3', 'PrevTldRank': '10', 'RefIPs': '6', 'RefSubNets': '5', 'IDN_TLD': '8',
-                           'GlobalRank': '1', 'PrevRefSubNets': '11'},
-                          {'PrevRefIPs': '12', 'TLD': 'com', 'IDN_Domain': '7', 'PrevGlobalRank': '9', 'TldRank': '2',
-                           'Domain': '3', 'PrevTldRank': '10', 'RefIPs': '6', 'RefSubNets': '5', 'IDN_TLD': '8',
-                           'GlobalRank': '1', 'PrevRefSubNets': '11'},
-                          {'PrevRefIPs': '12', 'TLD': 'au', 'IDN_Domain': '7', 'PrevGlobalRank': '9', 'TldRank': '2',
-                           'Domain': '3', 'PrevTldRank': '10', 'RefIPs': '6', 'RefSubNets': '5', 'IDN_TLD': '8',
-                           'GlobalRank': '1', 'PrevRefSubNets': '11'}]
-
-        split = DataFlow_Template.CountTLDs()
+        countTLDS = DataFlow_Template.CountTLDs()
         expected = [{'Count': 2, 'TLD': 'au'}, {'Count': 1, 'TLD': 'com'}]
-        result = split.expand(testCollection)
+        result = countTLDS.expand(self.testCollection)
         self.assertEqual(expected, result)
+
+    def test_AddDTLDDesc_shouldAddDescriptionToElement(self):
+
+        expected = {'Domain': '3',
+          'GlobalRank': '1',
+          'IDN_Domain': '7',
+          'IDN_TLD': '8',
+          'PrevGlobalRank': '9',
+          'PrevRefIPs': '12',
+          'PrevRefSubNets': '11',
+          'PrevTldRank': '10',
+          'RefIPs': '6',
+          'RefSubNets': '5',
+          'TLD': 'au',
+          'TLD_Desc': 'Australia',
+          'TldRank': '2'}
+
+        testInstance = DataFlow_Template.AddDTLDDesc()
+        result = testInstance.process(self.testCollection[0], { 'au' : 'Australia', 'us' : 'United States'})
+        self.assertDictEqual(expected, result[0])
 
 def main():
     unittest.main()
