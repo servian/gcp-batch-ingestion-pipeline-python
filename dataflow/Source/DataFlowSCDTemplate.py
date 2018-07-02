293dfbb728d910d1b4a3cfa59ff2cd707dd5ecca
ryanyuan <ryan.yuan@outlook.com>
2018-07-02 10:41:58 +1000

    1) Simplified the dataflow pipelines 2) Added new template to create scd staging table

--- dataflow/Source/DataFlowSCDTemplate.py
+++ dataflow/Source/DataFlowSCDTemplate.py
@@ -11,7 +11,7 @@ BUCKET = 'servian_melb_practice'
 DATASET = 'test_batch_servian_scd'
 SCHEMA = 'GlobalRank:INTEGER,TldRank:INTEGER,Domain:STRING,TLD:STRING,RefSubNets:INTEGER,RefIPs:INTEGER,IDN_Domain:STRING,' \
          'IDN_TLD:STRING,PrevGlobalRank:INTEGER,PrevTldRank:INTEGER,PrevRefSubNets:INTEGER,PrevRefIPs:INTEGER,MD5:STRING,StartDate:TIMESTAMP,EndDate:TIMESTAMP'
-defaultInputFile = 'gs://{0}/Sample_Data/majestic_thousand.csv'.format(BUCKET)
+defaultInputFile = 'gs://{0}/Sample_Data/majestic_thousand_staging.csv'.format(BUCKET)
 
 WRITE_IN_DATETIME = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
 
@@ -19,10 +19,9 @@ WRITE_IN_DATETIME = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
 def run(argv=None):
     logging.basicConfig(level=logging.INFO)
 
-
-    pipeline_args =[
+    pipeline_args = [
         '--project={0}'.format(PROJECT),
-        '--job_name=majesticmillion',
+        '--job_name=majesticmillion-staging',
         '--save_main_session',
         '--staging_location=gs://{0}/staging/'.format(BUCKET),
         '--temp_location=gs://{0}/temp/'.format(BUCKET),
@@ -30,32 +29,32 @@ def run(argv=None):
         '--runner=DataflowRunner',
         # '--template_location=gs://{0}/templates/majestic_million_template'.format(BUCKET),
         '--zone=australia-southeast1-a'
-      #  '--region=australia-southeast1',
-        ]
+        #  '--region=australia-southeast1',
+    ]
     pipeline_options = PipelineOptions(pipeline_args)
     pipeline_options.view_as(SetupOptions).save_main_session = True
     inbound_options = pipeline_options.view_as(FileLoader)
     input = inbound_options.inputFile
 
     with beam.Pipeline(options=pipeline_options) as p:
-
         # Extract records as dictionaries
-        records =(
-            p
-            | 'Read File' >> beam.io.ReadFromText(input,skip_header_lines=1)
-            | 'Parse CSV' >> beam.ParDo(Split()))
+        records = (
+                p
+                | 'Read File' >> beam.io.ReadFromText(input, skip_header_lines=1)
+                | 'Parse CSV' >> beam.ParDo(Split()))
 
         # Write all records to BigQuery
         (records
-            | 'Write Items BQ' >> beam.io.WriteToBigQuery(
-                '{0}:{1}.TopSites'.format(PROJECT, DATASET), # Enter your table name
-                schema=SCHEMA,
-                create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
-                write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE
-            )
-        )
+         | 'Write Items BQ' >> beam.io.WriteToBigQuery(
+                    '{0}:{1}.TopSites_Staging'.format(PROJECT, DATASET),  # Enter your table name
+                    schema=SCHEMA,
+                    create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
+                    write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE
+                )
+         )
 
     p.run()
+
 
 # Class to split the CSV file and format as per the Schema
 class Split(beam.DoFn):
@@ -79,7 +78,6 @@ class FileLoader(PipelineOptions):
                                            help='Input file to process'
                                            )
 
+
 if __name__ == '__main__':
-   run()
-
-
+    run()
